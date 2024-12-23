a49ed76f2a4a104507ce88581d7ba59e0d8573d5
Sigfrid Dusci <sig@inforlife.ch>
2024-12-23 22:31:46 +0100

    Add debug

--- action.rb
+++ action.rb
@@ -12,10 +12,12 @@ begin
    #   throw :feedback, "I'm sorry, but the repo #{GITHUB_REPOSITORY} does not exist."
    # end
 
+    p "github.exist_milestone? #{github.exist_milestone?}"
     unless github.exist_milestone?
       throw :feedback, "The milestone #{GITHUB_MILESTONE} does not exist."
     end
 
+    p "github.all_issues_closed? #{github.all_issues_closed?}"
     unless github.all_issues_closed?
       throw :feedback, "The milestone #{GITHUB_MILESTONE} can't be released since it has at least an open issue."
     end
@@ -24,14 +26,17 @@ begin
     #   throw :feedback, "The milestone #{GITHUB_MILESTONE} can't be released since it has at least an issue doesn't have the expected labels."
     # end
 
+    p "github.missing_tag? #{github.missing_tag?}"
     unless github.missing_tag?
       throw :feedback, "The tag #{GITHUB_MILESTONE} already exists."
     end
 
+    p "github.required_checks_pass? #{github.required_checks_pass?}"
     unless github.required_checks_pass?
       throw :feedback, "At least a required check didn't pass during the last run on the default branch."
     end
 
+    p "github.release_milestone? #{github.release_milestone}"
     if github.release_milestone
       throw :feedback, "The release `#{GITHUB_REPOSITORY}:#{GITHUB_MILESTONE}` has been drafted at https://github.com/#{GITHUB_REPOSITORY}/releases/tag/#{GITHUB_MILESTONE}."
     end
