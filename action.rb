a9cdca426b2365b895cba16b12b0aa440f414917
Sigfrid Dusci <sig@inforlife.ch>
2024-12-23 22:19:30 +0100

    Add additional checks

--- action.rb
+++ action.rb
@@ -13,14 +13,26 @@ begin
    # end
 
     unless github.exist_milestone?
-      throw :feedback, "I'm sorry, but the milestone #{GITHUB_MILESTONE} does not exist."
+      throw :feedback, "The milestone #{GITHUB_MILESTONE} does not exist."
     end
 
     unless github.all_issues_closed?
-      throw :feedback, "I'm sorry, but the milestone #{GITHUB_MILESTONE} can't be released since it has at least an open issue."
+      throw :feedback, "The milestone #{GITHUB_MILESTONE} can't be released since it has at least an open issue."
     end
 
-    if true
+    # unless github.all_issues_labeled?
+    #   throw :feedback, "The milestone #{GITHUB_MILESTONE} can't be released since it has at least an issue doesn't have the expected labels."
+    # end
+
+    unless github.missing_tag?
+      throw :feedback, "The tag #{GITHUB_MILESTONE} already exists."
+    end
+
+    unless github.required_checks_pass?
+      throw :feedback, "At least a required check didn't pass during the last run on the default branch."
+    end
+
+    if github.release_milestone
       throw :feedback, "The release `#{GITHUB_REPOSITORY}:#{GITHUB_MILESTONE}` has been drafted at https://github.com/#{GITHUB_REPOSITORY}/releases/tag/#{GITHUB_MILESTONE}."
     end
   end
--- lib/git_hub.rb
+++ lib/git_hub.rb
@@ -7,7 +7,7 @@ class GitHub
     @github_repository = repo
     @milestone = milestone
     @version = milestone # !!!!
-    @client ||=  Octokit::Client.new(access_token: ENV.fetch("GITHUB_ACCESS_TOKEN"))
+    @client ||= Octokit::Client.new(access_token: ENV.fetch("GITHUB_ACCESS_TOKEN"))
   end
 
   #def exist_repo?
@@ -22,10 +22,35 @@ class GitHub
     gh_milestone[:open_issues] == 0
   end
 
+  def missing_tag?
+    @client.tags(@github_repository).none? { |repo_tag| repo_tag[:name] == @milestone }
+  end
+
+  def required_checks_pass?
+    @client.check_runs_for_ref(@github_repository, dafault_branch)
+           .check_runs.select { |check| required_status_checks.include?(check[:name]) }
+           .all? { |check| check[:status] == "completed" && check[:conclusion] == "success" }
+  end
+
+  def release_milestone
+   # create_release
+   # close_milestone
+   # Documentation.new(@github_repository).publish
+    true
+  end
+
   private
 
   def gh_milestone
     @gh_milestone ||= @client.milestones(@github_repository, state: "all")
                              .detect { |repo_milestone| repo_milestone[:title] == @milestone }
+  end
+
+  def required_status_checks
+    @client.branch_protection(@github_repository, dafault_branch)[:required_status_checks][:contexts]
+  end
+
+  def dafault_branch
+    @client.repository(@github_repository)[:default_branch]
   end
 end
