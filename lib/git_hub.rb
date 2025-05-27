82872dceaae480656439022f741bacbaa251fc4e
Sigfrid Dusci <sigfrid@users.noreply.github.com>
2025-05-27 10:02:21 +0200

    Update git_hub.rb

--- lib/git_hub.rb
+++ lib/git_hub.rb
@@ -74,6 +74,7 @@ class GitHub
   def create_release
       body = "## What's Changed\n"
       changelog_issues.each do |issue|
+        p issue
         body += "* #{issue[:title]} by USER in PULL_URL\n"
       end
       body += "\n\n**Full Changelog**: https://github.com/#{@github_repository}/commits/#{@milestone}"
