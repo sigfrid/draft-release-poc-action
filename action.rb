cfbec4dc914c6647a055b489dabed6e02292226e
Sigfrid Dusci <sig@inforlife.ch>
2025-05-28 10:33:33 +0200

    Update

--- action.rb
+++ action.rb
@@ -3,48 +3,38 @@ require_relative "lib/git_hub"
 GITHUB_REPOSITORY = ENV.fetch("GITHUB_REPOSITORY")
 GITHUB_MILESTONE = ENV.fetch("GITHUB_MILESTONE")
 
-
 github = GitHub.new(repo: GITHUB_REPOSITORY, milestone: GITHUB_MILESTONE)
 
 begin
   feedback = catch(:feedback) do
-    exit 4
-    # THE ACTION RUNS ON THE REPO !!!
-    #unless github.exist_repo?
-    #  throw :feedback, "I'm sorry, but the repo #{GITHUB_REPOSITORY} does not exist."
-    #end
-=begin
-    p "github.exist_milestone? #{github.exist_milestone?}"
     unless github.exist_milestone?
-      throw :feedback, "The milestone #{GITHUB_MILESTONE} does not exist."
+      throw :feedback, "Milestone #{GITHUB_MILESTONE} does not exist."
     end
 
-    p "github.all_issues_closed? #{github.all_issues_closed?}"
     unless github.all_issues_closed?
-      throw :feedback, "The milestone #{GITHUB_MILESTONE} can't be released since it has at least an open issue."
+      throw :feedback, "At least an issue is still open."
     end
 
-    # unless github.all_issues_labeled?
-    #   throw :feedback, "The milestone #{GITHUB_MILESTONE} can't be released since it has at least an issue doesn't have the expected labels."
-    # end
+    unless github.all_issues_labeled?
+      throw :feedback, "At least an issue doesn't have the required labels."
+    end
 
-    p "github.missing_tag? #{github.missing_tag?}"
     unless github.missing_tag?
-      throw :feedback, "The tag #{GITHUB_MILESTONE} already exists."
+      throw :feedback, "Tag #{GITHUB_MILESTONE} already exists."
     end
 
-    p "github.required_checks_pass? #{github.required_checks_pass?}"
     unless github.required_checks_pass?
       throw :feedback, "At least a required check didn't pass during the last run on the default branch."
     end
-=end
-   # p "github.release_milestone? #{github.release_milestone}"
-    #if github.release_milestone
-    #  throw :feedback, "The release `#{GITHUB_REPOSITORY}:#{GITHUB_MILESTONE}` has been drafted at https://github.com/#{GITHUB_REPOSITORY}/releases/tag/#{GITHUB_MILESTONE}."
-    #end
   end
 
-  puts feedback
+  unless feedback.nil?
+    puts feedback
+    exit 1
+  else
+    puts "All checks passed. #{GITHUB_MILESTONE} can be released."
+    exit 0
+  end
 
 rescue StandardError => error
   puts "I'm sorry, but #{error.message}."
--- lib/git_hub.rb
+++ lib/git_hub.rb
@@ -1,92 +1,55 @@
 # frozen_string_literal: true
 
-require 'octokit'
+require "octokit"
 
 class GitHub
+  REQUIRED_LABELS = %w[accepted approved confirmed].freeze
+
   def initialize(repo:, milestone:)
-    @github_repository = repo
-    @milestone = milestone
-    #@version = milestone # !!!!
+    @repository = repo
+    @milestone_title = milestone
     @client ||= Octokit::Client.new(access_token: ENV.fetch("GITHUB_ACCESS_TOKEN"))
   end
 
-  #def exist_repo?
-  #  @client.repository?(@github_repository)
-  #end
-
   def exist_milestone?
-    gh_milestone != nil
+    milestone != nil
   end
 
   def all_issues_closed?
-    gh_milestone[:open_issues] == 0
+    milestone[:open_issues] == 0
+  end
+
+  def all_issues_labeled?
+      milestone_issues.map { |issue| issue[:labels].map { |label| label[:name] } }
+                      .all? { |label_list| (REQUIRED_LABELS & label_list) == REQUIRED_LABELS }
   end
 
   def missing_tag?
-    @client.tags(@github_repository).none? { |repo_tag| repo_tag[:name] == @milestone }
+    @client.tags(@repository).none? { |tag| tag[:name] == @milestone_title }
   end
 
   def required_checks_pass?
-    return true
-    @client.check_runs_for_ref(@github_repository, dafault_branch)
+    @client.check_runs_for_ref(@repository, dafault_branch)
            .check_runs.select { |check| required_status_checks.include?(check[:name]) }
            .all? { |check| check[:status] == "completed" && check[:conclusion] == "success" }
   end
 
-  def release_milestone
-     create_release
-     close_milestone
-   # Documentation.new(@github_repository).publish
-    true
-  end
-
   private
 
-  def gh_milestone
-    @gh_milestone ||= @client.milestones(@github_repository, state: "all")
-                             .detect { |repo_milestone| repo_milestone[:title] == @milestone }
-  end
-
-  def required_status_checks
-    @client.branch_protection(@github_repository, dafault_branch)[:required_status_checks][:contexts]
-  end
-
-  def dafault_branch
-    @client.repository(@github_repository)[:default_branch]
-  end
-
-  def close_milestone
-    @client.update_milestone(@github_repository, gh_milestone[:number], { state: 'closed' })
-  end
-
-  def gh_milestone
-    @gh_milestone ||= @client.milestones(@github_repository, state: 'all').detect { |repo_milestone| repo_milestone[:title] == @milestone }
+  def milestone
+    @milestone ||= @client.milestones(@repository, state: "all")
+                             .detect { |milestone| milestone[:title] == @milestone_title }
   end
 
   def milestone_issues
-      @client.issues(@github_repository, milestone: gh_milestone[:number], state: 'all')
-  end
-  
-  def changelog_issues
-    milestone_issues#.reject { |issue| !(issue[:labels].map(&:name) & ENV['GITHUB_LABELS_TO_IGNORE'].split('#')).empty? }
+    @client.issues(@repository, milestone: milestone[:number], state: "all")
   end
 
-  def create_release
-     # body = "## What's Changed\n"
-     # changelog_issues.each do |issue|
-      #  p issue[:pull_request]
-      #  body += "* #{issue[:title]} by USER in PULL_URL\n"
-      #end
-      #body += "\n\n**Full Changelog**: https://github.com/#{@github_repository}/commits/#{@milestone}"
-      
+  def required_status_checks
+    @client.branch_protection(@repository, dafault_branch)[:required_status_checks][:contexts]
+  end
 
-      @client.create_release(@github_repository, @milestone, { target_commitish: dafault_branch, name: @milestone }) #, body: body })
-    end
+  def dafault_branch
+    @dafault_branch ||= @client.repository(@repository)[:default_branch]
+  end
 end
-
-
-### What's Changed
-#* Build by @sigfrid in https://github.com/inforlife/fortivpn/pull/1
-#* Add arm64 by @sigfrid in https://github.com/inforlife/fortivpn/pull/2
-
-#**Full Changelog**: https://github.com/inforlife/fortivpn/commits/1.0.0
